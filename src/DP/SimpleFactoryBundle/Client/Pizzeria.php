3bf83a02751586328b98b4d5204b2de91d133e12
FFJM <ffjm_inc@yahoo.fr>
2015-06-01 20:42:40 +0200

    Utilisation de la factory en static

--- src/DP/SimpleFactoryBundle/Client/Pizzeria.php
+++ src/DP/SimpleFactoryBundle/Client/Pizzeria.php
@@ -7,20 +7,16 @@ use DP\SimpleFactoryBundle\Factory\SimpleFabriqueDePizzas;
 class Pizzeria
 {
     /**
-     * var \DP\SimpleFactoryBundle\Factory\SimpleFabriqueDePizzas
-     *
+     * Passage de commande
+     * @param string $type Type de pizza
+     * @param array $add ingredients à ajouter
+     * @param array $subst ingredients à retirer
+     * @return Pizza
      */
-    private $fabrique;
-
-    public function __construct(SimpleFabriqueDePizzas $fabrique)
+    public function commanderPizza($type,$add = array(),$subst = array())
     {
-        $this->fabrique = $fabrique;
-    }
-
-    public function commanderPizza($type)
-    {
-        $pizza = $this->fabrique->creerPizza($type);
-        
+        $pizza = SimpleFabriqueDePizzas::creerPizza($type,array('add'   => $add,
+                                                                 'subst'=> $subst));      
         return $pizza;
     }
 
